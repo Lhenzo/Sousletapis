b4ad3630eab1811433250c67fec89d3afe74a391
Lhenzo <lorenzo.furlan.pro@gmail.com>
2021-03-31 15:28:17 +0200

    Added Project - 30.03.21
    Added project received 30.03.21 from Jules

---
+++ SousLeTapis/Assets/First person controller/FirstPersonMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+    Vector2 velocity;
+    private Animator m_animator;
+    private void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        velocity.y = Input.GetAxis("Vertical") * speed;
+        velocity.x = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(velocity.x * Time.deltaTime, 0, velocity.y * Time.deltaTime);
+    }
+    private void Update()
+    {
+        if (m_animator != null) 
+        {
+            m_animator.SetFloat("Speed", velocity.magnitude);
+        }
+    }
+}
